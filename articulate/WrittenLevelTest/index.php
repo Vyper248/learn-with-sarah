bc4032809f93022bc151100483a5c23c6a462e21
Vyper248 <vyper375@hotmail.com>
2018-12-18 16:45:48 +0000

    added new pages and updated links

---
+++ articulate/WrittenLevelTest/index.php
@@ -0,0 +1,7 @@
+<?php include '../templateTop.php'; ?>
+
+<div class="content">
+    <iframe width="1000" height="680" src="story_html5.html" class="embedded"></iframe>
+</div>
+
+<?php include '../templateBottom.php'; ?>
